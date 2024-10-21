d23b0ccb57cce4808911672226d6b51002f395c6
grafica <paolo@monema.it>
2024-10-21 11:08:26 +0200

    messaggio 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,21 @@
 "use client";
-
-import data404 from "../../public/data/404.json";
-
 function NotFound() {
   return (
-    <main>
-      <section className="p404">
-        <h1>Pagina non trovata</h1>
+    <main style={{ textAlign: "center" }}>
+      <section
+        className="p404"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "60vh",
+        }}
+      >
+        <a href="/">
+          <h1 style={{ border: "1px solid", padding: "2rem" }}>
+            La pagina che stai cercando non esiste!
+          </h1>
+        </a>
       </section>
     </main>
   );
